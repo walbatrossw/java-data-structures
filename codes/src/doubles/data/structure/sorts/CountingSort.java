f65d805e5d17203c77a11e6daba504f4805c5d22
SangSeob Yoon <walbatrossw@gmail.com>
2018-05-06 17:12:38 +0900

    12th commit : sort 구현중...

--- codes/src/doubles/data/structure/sorts/CountingSort.java
+++ codes/src/doubles/data/structure/sorts/CountingSort.java
@@ -1,4 +1,31 @@
 package doubles.data.structure.sorts;
 
 public class CountingSort {
+    public static void main(String[] args) {
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+        countingSort(intArray, 1, 10);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void countingSort(int[] input, int min, int max) {
+
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+
+    }
 }
--- codes/src/doubles/data/structure/sorts/QuickSort.java
+++ codes/src/doubles/data/structure/sorts/QuickSort.java
@@ -1,4 +1,55 @@
 package doubles.data.structure.sorts;
 
 public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        quickSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+        }
+        quickSort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot) ;
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot) ;
+            if (i < j) {
+                input[j] = input[i];
+            }
+
+        }
+
+        input[j] = pivot;
+        return j;
+
+    }
+
 }
