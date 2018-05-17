7b6ed971fb1d2ba6f7c8d8aa6d94a9efdbd6e43f
SangSeob Yoon <walbatrossw@gmail.com>
2018-05-17 22:51:37 +0900

    26th commit : 기존 코드 제거

--- stacks/src/doubles/stacks/linkedlist/LinkedListStack.java
+++
@@ -1,5 +0,0 @@
-package doubles.stacks.linkedlist;
-
-public class LinkedListStack {
-
-}
--- stacks/src/doubles/stacks/linkedlist/LinkedListStackMain.java
+++
@@ -1,9 +0,0 @@
-package doubles.stacks.linkedlist;
-
-public class LinkedListStackMain {
-
-    public static void main(String[] args) {
-
-    }
-
-}
