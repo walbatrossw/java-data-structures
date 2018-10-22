4dcfaa775efbd3f8fd3cf617c2cb47647f5cdcf7
SangSeob Yoon <walbatrossw@gmail.com>
2018-10-22 22:45:50 +0900

    87th commit : binary search tree 구현2

--- ch05-binary-search-trees/src/doubles/ds/bst/Tree.java
+++ ch05-binary-search-trees/src/doubles/ds/bst/Tree.java
@@ -1,9 +1,11 @@
 package doubles.ds.bst;
 
+import doubles.ds.bst.implement.Node;
+
 public interface Tree<T> {
 
     public void traversal();
-    public void insert(T data);
+    public void insertNode(T data, Node<T> node);
     public void delete(T data);
     public T getMax();
     public T getMin();
--- ch05-binary-search-trees/src/doubles/ds/bst/implement/BinarySearchTree.java
+++ ch05-binary-search-trees/src/doubles/ds/bst/implement/BinarySearchTree.java
@@ -1,4 +1,60 @@
 package doubles.ds.bst.implement;
 
-public class BinarySearchTree {
+import doubles.ds.bst.Tree;
+
+public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
+
+    private Node<T> root;
+
+    public void insert(T data) {
+        // 루트노드 삽입
+        if (root == null) {
+            root = new Node<>(data);
+        // 하위노드 삽입
+        } else {
+            insertNode(data, root);
+        }
+    }
+
+    @Override
+    public void traversal() {
+
+    }
+
+    @Override
+    public void insertNode(T newData, Node<T> node) {
+        //
+        if (newData.compareTo(node.getData()) < 0) {
+            if (node.getLeftChild() != null) {
+                insertNode(newData, node.getLeftChild());
+            } else {
+                Node<T> newNode = new Node<>(newData);
+                node.setLeftChild(newNode);
+            }
+
+        } else {
+            if (node.getRightChild() != null) {
+                insertNode(newData, node.getRightChild());
+            } else {
+                Node<T> newNode = new Node<>(newData);
+                node.setRightChild(newNode);
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(T data) {
+
+    }
+
+    @Override
+    public T getMax() {
+        return null;
+    }
+
+    @Override
+    public T getMin() {
+        return null;
+    }
 }
