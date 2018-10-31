2fdb44ac1d6f20c696235c027dbd6045f53ebc2d
SangSeob Yoon <walbatrossw@gmail.com>
2018-10-31 11:42:05 +0900

    113rd commit : avl tree 정리 13 - 탐색 이미지 업로드

--- ch06-avl-trees/src/doubles/ds/avltree/implement/integer/AVLTree.java
+++ ch06-avl-trees/src/doubles/ds/avltree/implement/integer/AVLTree.java
@@ -10,20 +10,6 @@ public class AVLTree implements Tree {
 
     }
 
-    public int height(Node node) {
-        if (node == null) {
-            return -1;
-        }
-        return node.getHeight();
-    }
-
-    public int getBalance(Node node) {
-        if (node == null) {
-            return 0;
-        }
-        return height(node.getLeftNode()) - height(node.getRightNode());
-    }
-
     // 순회
     @Override
     public void traverse() {
@@ -31,24 +17,76 @@ public class AVLTree implements Tree {
         if (root == null) {
             return;
         }
-
         inorderTraversal(root);
+        preOrderTraversal(root);
+        postOrderTraversal(root);
+    }
+
+    // 오른쪽 회전
+    private Node rightRotation(Node node) {
+        System.out.println("Rotating to the right...");
+        Node tempLeftNode = node.getLeftNode();
+        Node t = tempLeftNode.getRightNode();
+
+        tempLeftNode.setRightNode(node);
+        node.setLeftNode(t);
+        node.setHeight(Math.max(height(node.getLeftNode()), height(node.getRightNode())) + 1);
+        tempLeftNode.setHeight(Math.max(height(tempLeftNode.getLeftNode()), height(tempLeftNode.getRightNode())) + 1);
+        return tempLeftNode;
+    }
+
+    // 왼쪽 회전
+    private Node leftRotation(Node node) {
 
     }
+
+
+    // 특정 노드의 높이 반환
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.getHeight();
+    }
+
+    // 트리 균형, 불균형 여부 판단
+    private int getBalance(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.getLeftNode()) - height(node.getRightNode());
+    }
+
 
     // 중위 순회
     private void inorderTraversal(Node node) {
-
         if (node.getLeftNode() != null) {
             inorderTraversal(node.getLeftNode());
         }
-
         System.out.println(node);
-
         if (node.getRightNode() != null) {
             inorderTraversal(node.getRightNode());
         }
-
     }
 
+    // 전위 순회
+    private void preOrderTraversal(Node node) {
+        System.out.println(node);
+        if (node.getLeftNode() != null) {
+            inorderTraversal(node.getLeftNode());
+        }
+        if (node.getRightNode() != null) {
+            inorderTraversal(node.getRightNode());
+        }
+    }
+
+    private void postOrderTraversal(Node node) {
+        if (node.getLeftNode() != null) {
+            inorderTraversal(node.getLeftNode());
+        }
+        if (node.getRightNode() != null) {
+            inorderTraversal(node.getRightNode());
+        }
+        System.out.println(node);
+    }
 }
