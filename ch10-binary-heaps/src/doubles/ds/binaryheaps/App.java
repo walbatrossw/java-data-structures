67208a7217a733399c4856c2ce06d45657b56943
walbatrossw <walbatrossw@gmail.com>
2019-02-21 23:59:23 +0900

    132nd commit : binary heap 코드 및 주석 작성중...

--- ch10-binary-heaps/src/doubles/ds/binaryheaps/App.java
+++ ch10-binary-heaps/src/doubles/ds/binaryheaps/App.java
@@ -1,8 +1,10 @@
 package doubles.ds.binaryheaps;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        Heap heap = new Heap(10);
+        Heap heap = new Heap(8);
         heap.insert(20);
         heap.insert(4);
         heap.insert(45);
@@ -10,6 +12,14 @@ public class App {
         heap.insert(7);
         heap.insert(33);
         heap.insert(70);
+        heap.insert(35);
+        heap.heapSort();
+
         System.out.println(heap.getMax());
+//        System.out.println(heap.getMax());
+//        System.out.println(heap.getMax());
+//        System.out.println(heap.getMax());
+//        System.out.println(heap.getMax());
+//        System.out.println(heap.getMax());
     }
 }
--- ch10-binary-heaps/src/doubles/ds/binaryheaps/Heap.java
+++ ch10-binary-heaps/src/doubles/ds/binaryheaps/Heap.java
@@ -4,7 +4,7 @@ public class Heap {
 
     private Integer[] heap; // 데이터를 저장활 배열
 
-    private int currentPosition = -1;   // 데이터가 저장될 위치, -1f로 초기화
+    private int currentPosition = -1;   // 데이터가 저장된 위치, -1로 초기화
 
     // 생성자
     public Heap(int size) {
@@ -13,10 +13,13 @@ public class Heap {
 
     // 삽입
     public void insert(int item) {
+        // heap이 차있는지 확인
         if (isFull()) {
             throw new RuntimeException("Heap is Full...");
         }
+        // 항목 삽입
         this.heap[++currentPosition] = item;
+        // heap 속성 확인 및 속성에 충족하도록 수정처리
         fixUp(currentPosition);
     }
 
@@ -33,19 +36,24 @@ public class Heap {
     public void heapSort() {
         for (int i = 0; i <= currentPosition; i++) {
             int temp = this.heap[0];
-            System.out.println(temp + " ");
+            System.out.print(temp + " ");
             this.heap[0] = this.heap[currentPosition - i];
             this.heap[currentPosition - i] = temp;
             fixDown(0, currentPosition - i - 1);
         }
+        System.out.println();
     }
 
-
+    // Heap 속성에 맞게 상위 노드와 하위 노드의 위치 교환
     private void fixUp(int index) {
 
+        // 상위노드 인덱스 계산
         int parentIndex = (index - 1) / 2;
 
+        // 상위 노드가 루트 노드이고, 상위 노드가 더 클 때까지 반복 수행
         while (parentIndex >= 0 && this.heap[parentIndex] < this.heap[index]) {
+
+            // 현재노드와 부모노드 교환 수행
             int temp = this.heap[index];
             this.heap[index] = this.heap[parentIndex];
             this.heap[parentIndex] = temp;
@@ -54,6 +62,7 @@ public class Heap {
         }
 
     }
+
 
     private void fixDown(int index, int upto) {
         if (upto < 0) {
@@ -89,6 +98,7 @@ public class Heap {
         }
     }
 
+    // Heap이 차있는지 확인
     private boolean isFull() {
         return this.currentPosition == this.heap.length;
     }
